0796bdb6b57b1f15c760e1d1f74dbd6b439361c8
RaghavGohil <raghav15102004@gmail.com>
2022-09-07 16:07:28 +0530

    -movement mechanic complete (click and drag) -some minor checks remaining

--- src/main.py
+++ src/main.py
@@ -1,3 +1,4 @@
+from typing import Any
 import os
 import pygame
 from pygame.locals import *
@@ -40,16 +41,39 @@ def quit_game():
     l_audio.pquit()
     quit()
 
-def move_piece():
-    for e in events:
-        if e.type == pygame.KEYDOWN:
-            if e.key == pygame.K_w:
-                board.w_pawn = ['e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 
-'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'p', 'e', 'p', 'p', 'p', 'p', 'p', 'p', 'e', 'p', 'e', 'e', 'e', 'e', 'e', 'e', 'e', 'e']
-                l_audio.play(4,0)
-                board.update_board()
+# game variables
+piece_is_held = False
+offsetx,offsety = 0,0
+attached_pieces = []
 
-def update_pieces(): # very performance heavy!
+def move_piece():
+    global piece_is_held,offsetx,offsety,attached_pieces
+    mousepos = pygame.mouse.get_pos()
+    pressed = pygame.mouse.get_pressed()
+
+    for e in events:
+        for p in pieces:
+            if((mousepos[0] > p.position[0] and mousepos[0] < (p.position[0]+board.isps)) and (mousepos[1] > p.position[1] and mousepos[1] < (p.position[1]+board.isps))):
+                if pressed[0] and not piece_is_held:
+                    piece_is_held = True
+                    if len(attached_pieces) < 1:
+                        attached_pieces.append(p)
+                    offsetx = (mousepos[0]-attached_pieces[0].position[0])
+                    offsety = (mousepos[1]-attached_pieces[0].position[1])
+                    # piece_prev_pos[0] = p.position[0]
+                    # piece_prev_pos[1] = p.position[1]
+                if not pressed[0] and piece_is_held:      
+                        piece_is_held = False
+                        attached_pieces[0].position[0] = border.position[0]
+                        attached_pieces[0].position[1] = border.position[1] # wierd but works lmao
+                        attached_pieces.pop(0)
+                        offsetx,offsety = 0,0
+                        l_audio.play(4,0)
+        if e.type == MOUSEMOTION and piece_is_held:
+                    attached_pieces[0].position[0] = mousepos[0]-offsetx
+                    attached_pieces[0].position[1] = mousepos[1]-offsety
+
+def initialize_pieces(): # very performance heavy!
     global pieces
 
     pieces = []
@@ -59,29 +83,29 @@ def update_pieces(): # very performance heavy!
             if(board.char_board[y+(x*8)] == 'e'):
                 continue
             elif(board.char_board[y+(x*8)] == 'P'):
-                pieces.append(l_piece.Piece((y*75,x*75),l_settings.base_path,'P'))
+                pieces.append(l_piece.Piece([y*board.isps,x*board.isps],l_settings.base_path,'P'))
             elif(board.char_board[y+(x*8)] == 'R'):
-                pieces.append(l_piece.Piece((y*75,x*75),l_settings.base_path,'R'))
+                pieces.append(l_piece.Piece([y*board.isps,x*board.isps],l_settings.base_path,'R'))
             elif(board.char_board[y+(x*8)] == 'N'):
-                pieces.append(l_piece.Piece((y*75,x*75),l_settings.base_path,'N'))
+                pieces.append(l_piece.Piece([y*board.isps,x*board.isps],l_settings.base_path,'N'))
             elif(board.char_board[y+(x*8)] == 'B'):
-                pieces.append(l_piece.Piece((y*75,x*75),l_settings.base_path,'B'))
+                pieces.append(l_piece.Piece([y*board.isps,x*board.isps],l_settings.base_path,'B'))
             elif(board.char_board[y+(x*8)] == 'Q'):
-                pieces.append(l_piece.Piece((y*75,x*75),l_settings.base_path,'Q'))
+                pieces.append(l_piece.Piece([y*board.isps,x*board.isps],l_settings.base_path,'Q'))
             elif(board.char_board[y+(x*8)] == 'K'):
-                pieces.append(l_piece.Piece((y*75,x*75),l_settings.base_path,'K'))
+                pieces.append(l_piece.Piece([y*board.isps,x*board.isps],l_settings.base_path,'K'))
             elif(board.char_board[y+(x*8)] == 'p'):
-                pieces.append(l_piece.Piece((y*75,x*75),l_settings.base_path,'p'))
+                pieces.append(l_piece.Piece([y*board.isps,x*board.isps],l_settings.base_path,'p'))
             elif(board.char_board[y+(x*8)] == 'r'):
-                pieces.append(l_piece.Piece((y*75,x*75),l_settings.base_path,'r'))
+                pieces.append(l_piece.Piece([y*board.isps,x*board.isps],l_settings.base_path,'r'))
             elif(board.char_board[y+(x*8)] == 'n'):
-                pieces.append(l_piece.Piece((y*75,x*75),l_settings.base_path,'n'))
+                pieces.append(l_piece.Piece([y*board.isps,x*board.isps],l_settings.base_path,'n'))
             elif(board.char_board[y+(x*8)] == 'b'):
-                pieces.append(l_piece.Piece((y*75,x*75),l_settings.base_path,'b'))
+                pieces.append(l_piece.Piece([y*board.isps,x*board.isps],l_settings.base_path,'b'))
             elif(board.char_board[y+(x*8)] == 'q'):
-                pieces.append(l_piece.Piece((y*75,x*75),l_settings.base_path,'q'))
+                pieces.append(l_piece.Piece([y*board.isps,x*board.isps],l_settings.base_path,'q'))
             elif(board.char_board[y+(x*8)] == 'k'):
-                pieces.append(l_piece.Piece((y*75,x*75),l_settings.base_path,'k'))
+                pieces.append(l_piece.Piece([y*board.isps,x*board.isps],l_settings.base_path,'k'))
 
 def display_pieces(): # i haven't looped all this nonsense because of readablility issues
     for x in pieces:
@@ -93,11 +117,11 @@ def init():
     l_audio.play(0,0) #play start sound
     board = l_board.Board(l_settings.base_path,(winsize[1],winsize[1]))
     border = l_border.Border(l_settings.base_path)
+    initialize_pieces()
 
 def main():
     board.display(screen)
     border.display(screen,board)
-    update_pieces()
     move_piece()
     display_pieces()
     l_in_game_locator.render_current_in_game_location(screen,initialized_font,board,l_colors.current_location_font_bounding_box_color,l_colors.current_location_font_color,winw,winh,l_font.font_antialiasing)
